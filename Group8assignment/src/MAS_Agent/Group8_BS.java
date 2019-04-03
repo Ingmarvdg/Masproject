0efd71491c62d36df196576cdf521feddd773e7f
Unknown <diego-s-@hotmail.com>
2019-04-03 13:00:54 +0200

    deleted redundant code from BS, debugged rejection to AS when in last turn, Changed name to THOTIANA

--- Group8assignment/src/MAS_Agent/Group8_BS.java
+++ Group8assignment/src/MAS_Agent/Group8_BS.java
@@ -5,19 +5,14 @@ package MAS_Agent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
-import MAS_Agent.help_components.CUHKAgent_Offering;
 import MAS_Agent.help_components.Fawkes_Offering;
 import MAS_Agent.help_components.TheNegotiatorReloaded_Offering;
 import genius.core.Bid;
 import genius.core.bidding.BidDetails;
-import genius.core.boaframework.BOAparameter;
 import genius.core.boaframework.NegotiationSession;
 import genius.core.boaframework.OMStrategy;
 import genius.core.boaframework.OfferingStrategy;
@@ -31,19 +26,9 @@ import genius.core.utility.AbstractUtilitySpace;
  * @author Andrea Scorza, Diego Staphorst, Erik Lokhorst, Ingmar van der Geest
  *
  */
-public class Group8_BS extends OfferingStrategy {
 
-//	/**
-//	 * k in [0, 1]. For k = 0 the agent starts with a bid of maximum utility
-//	 */
-	private double k;
-	/** Maximum target utility */
-	private double Pmax;
-	/** Minimum target utility */
-	private double Pmin;
-	/** Concession factor */
-	private double e;
-	/** Outcome space */
+public class Group8_BS extends OfferingStrategy {
+//	/** Outcome space */
 	private SortedOutcomeSpace outcomespace;
 	
 	/** Bidding strategies */
@@ -55,15 +40,14 @@ public class Group8_BS extends OfferingStrategy {
 	/** Bidding utility space of current negotiation */
 	private AbstractUtilitySpace abstractUtilitySpace;
 	
-	
+
 public Group8_BS() {
 		// TODO Auto-generated constructor stub
 	}
 
-	//	/**
-//	 * Method which initializes the agent by setting all parameters. The
-//	 * parameter "e" is the only parameter which is required.
-//	 */
+	/**
+ 	* Initiate agents, weights for the bidding strategy
+ 	*/
 	@Override
 	public void init(NegotiationSession negoSession, OpponentModel model, OMStrategy oms,
 			Map<String, Double> parameters) throws Exception {
@@ -82,24 +66,6 @@ public Group8_BS() {
 		bid_weights.add(0.33);
 		bid_weights.add(0.33);
 
-		this.e = parameters.get("e");
-
-		if (parameters.get("k") != null)
-			this.k = parameters.get("k");
-		else
-			this.k = 0;
-
-		if (parameters.get("min") != null)
-			this.Pmin = parameters.get("min");
-		else
-			this.Pmin = negoSession.getMinBidinDomain().getMyUndiscountedUtil();
-
-		if (parameters.get("max") != null) {
-			Pmax = parameters.get("max");
-		} else {
-			BidDetails maxBid = negoSession.getMaxBidinDomain();
-			Pmax = maxBid.getMyUndiscountedUtil();
-		}
 		
 		outcomespace = new SortedOutcomeSpace(negotiationSession.getUtilitySpace());
 		negotiationSession.setOutcomeSpace(outcomespace);		
@@ -117,11 +83,6 @@ public Group8_BS() {
 	 */
 	@Override
 	public BidDetails determineNextBid() {
-		double time = negotiationSession.getTime();
-		double utilityGoal;
-		utilityGoal = p(time);
-
-
 		// Pools all bids, separated in issues, with its value and determined
 		// weight for this bid per agent in the variable bids
 		HashMap<Issue, HashMap<Value, Double>> bids = new HashMap<Issue, HashMap<Value, Double>>();		
@@ -157,62 +118,22 @@ public Group8_BS() {
 			bid_to_determine.put(i, value_with_heighest_weight);
 			i++;
 		}
-		
-		double utility_bid =  this.abstractUtilitySpace.getUtility(new Bid(negotiationSession.getDomain(), bid_to_determine));
-		System.out.println(utility_bid);
-		
-		nextBid = new BidDetails(new Bid(negotiationSession.getDomain(), bid_to_determine), utilityGoal);
+		// Our Acceptance strategy is based on the Utility we offer to the other agent
+		double utility_bid =  this.abstractUtilitySpace.getUtility(new Bid(negotiationSession.getDomain(), bid_to_determine));		
+		nextBid = new BidDetails(new Bid(negotiationSession.getDomain(), bid_to_determine), utility_bid);
 		return nextBid;
 	}
 
-	/**
-	 * From [1]:
-	 * 
-	 * A wide range of time dependent functions can be defined by varying the
-	 * way in which f(t) is computed. However, functions must ensure that 0 <=
-	 * f(t) <= 1, f(0) = k, and f(1) = 1.
-	 * 
-	 * That is, the offer will always be between the value range, at the
-	 * beginning it will give the initial constant and when the deadline is
-	 * reached, it will offer the reservation value.
-	 * 
-	 * For e = 0 (special case), it will behave as a Hardliner.
-	 */
-	
-	public double f(double t) {
-		if (e == 0)
-			return k;
-		double ft = k + (1 - k) * Math.pow(t, 1.0 / e);
-		return ft;
-	}
 
 	/**
-	 * Makes sure the target utility with in the acceptable range according to
-	 * the domain. Goes from Pmax to Pmin!
-	 * 
-	 * @param t
-	 * @return double
-	 */
-	public double p(double t) {
-		
-		return Pmin + (Pmax - Pmin) * (1 - f(t));
-	}
-
+ 	* To determine the utility for the offer we make
+ 	*/
 	public void setAbstractUtilitySpace(AbstractUtilitySpace abstractUtilitySpace) {
 		this.abstractUtilitySpace = abstractUtilitySpace;
 	}
+
 	public NegotiationSession getNegotiationSession() {
 		return negotiationSession;
-	}
-
-	@Override
-	public Set<BOAparameter> getParameterSpec() {
-		Set<BOAparameter> set = new HashSet<BOAparameter>();
-		set.add(new BOAparameter("e", 1.0, "Concession rate"));
-		set.add(new BOAparameter("k", 0.0, "Offset"));
-		set.add(new BOAparameter("min", 0.0, "Minimum utility"));
-		set.add(new BOAparameter("max", 0.99, "Maximum utility"));
-		return set;
 	}
 
 	@Override
